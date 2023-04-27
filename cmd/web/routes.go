a9488754f92686eca31c70608881c71322580c0e
Timur Rakhmanov <rakhmanovtim@gmail.com>
2023-04-28 00:28:25 +0900

    Add server-side form validation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,7 +25,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	mux.Get("/contact", handlers.Repo.Contact)
+
 	mux.Get("/make-reservation", handlers.Repo.MakeReservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
+	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
