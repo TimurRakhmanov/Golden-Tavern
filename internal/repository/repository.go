602d2ef63a6437323f56e644ec1c2c2ad111ca39
Timur Rakhmanov <rakhmanovtim@gmail.com>
2023-05-05 16:15:36 +0900

    Define insertReservation function

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/RakhmanovTimur/bookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
