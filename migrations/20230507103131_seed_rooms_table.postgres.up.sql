c6a9c675289da3f0a28cf630b3a585f271de4099
Timur Rakhmanov <rakhmanovtim@gmail.com>
2023-05-07 19:34:39 +0900

    Add migrations for table room

---
+++ migrations/20230507103131_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Secret HQ','2023-05-01 00:00:00.000','2023-05-01 00:00:00.000'),
+	 ('New World','2023-05-01 00:00:00.000','2023-05-01 00:00:00.000');
