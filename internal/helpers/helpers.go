7cc42dba26fa1a5e233776751291d80adf17dadc
Timur Rakhmanov <rakhmanovtim@gmail.com>
2023-05-10 20:23:33 +0900

    Add comments to helper functions

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -15,13 +15,21 @@ func NewHelpers(a *config.AppConfig) {
 	app = a
 }
 
+// ClientError writes a client-side error into InfoLog
 func ClientError(w http.ResponseWriter, status int) {
 	app.InfoLog.Println("Client error with status of", status)
 	http.Error(w, http.StatusText(status), status)
 }
 
+// ServerError traces the server error and writes it to the info log
 func ServerError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.ErrorLog.Println(trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// isAuthenticated returns true if the user is logged in, or false otherwise
+func IsAuthenticated(r *http.Request) bool {
+	exists := app.Session.Exists(r.Context(), "user_id")
+	return exists
 }
