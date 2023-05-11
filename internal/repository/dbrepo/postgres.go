23ad8033477ca49788a77f4a1fb478750809ba9b
Timur Rakhmanov <rakhmanovtim@gmail.com>
2023-05-11 22:15:03 +0900

    Fix sql queries

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -221,8 +221,8 @@ func (m *postgresDBRepo) AllReservations() ([]models.Reservation, error) {
 	var reservations []models.Reservation
 	query := `
 	select 
-		r.id, r.first_name, r.last_name, r.email, r.phone, r
-		.start_date, r.end_date, r.room_id, r.created_at, r.updated_at, r.processed
+		r.id, r.first_name, r.last_name, r.email, r.phone, r.start_date, 
+		r.end_date, r.room_id, r.created_at, r.updated_at, r.processed,
 		rm.id, rm.room_name 
 	from 
 		reservations r left join rooms rm on (r.room_id = rm.id)
@@ -261,8 +261,8 @@ func (m *postgresDBRepo) AllNewReservations() ([]models.Reservation, error) {
 	var reservations []models.Reservation
 	query := `
 	select 
-		r.id, r.first_name, r.last_name, r.email, r.phone, r
-		.start_date, r.end_date, r.room_id, r.created_at, r.updated_at, 
+		r.id, r.first_name, r.last_name, r.email, r.phone, r.start_date, 
+		r.end_date, r.room_id, r.created_at, r.updated_at, 
 		rm.id, rm.room_name 
 	from 
 		reservations r left join rooms rm on (r.room_id = rm.id)
