5fd1b49edab8f319c8044cb2ad77d136c035ae48
Timur Rakhmanov <rakhmanovtim@gmail.com>
2023-05-11 20:50:36 +0900

    Add AllNewReservations functions to the repository

--- internal/repository/dbrepo/test_repo.go
+++ internal/repository/dbrepo/test_repo.go
@@ -109,3 +109,10 @@ func (m *testDBRepo) AllReservations() ([]models.Reservation, error) {
 
 	return reservations, nil
 }
+
+// AllNewReservations returns a slice of new reservations
+func (m *testDBRepo) AllNewReservations() ([]models.Reservation, error) {
+	var reservations []models.Reservation
+
+	return reservations, nil
+}
